a4f593a2fd2704752fad618b18aec86b1ca8eed7
Nana Amfo-Brobbey <nana.amfobrobbey@gmail.com>
2020-10-14 09:23:17 +0400

    Beginning of a new and updated bot

--- bot.py
+++ bot.py
@@ -1,23 +1,23 @@
 import discord
 from discord.ext import commands
 import random
+import json
 
 client = commands.Bot(command_prefix="!")
 
 class Slapper(commands.Converter):
-    async def convert(self, ctx, argument):
-        to_slap = random.choice(ctx.guild.members)
-        return '{0.author} slapped {1} because *{2}*'.format(ctx, to_slap, argument)
-
+	async def convert(self, ctx, argument):
+		to_slap = random.choice(ctx.guild.members)
+		return '{0.author} slapped {1} because *{2}*'.format(ctx, to_slap, argument)
 
 @client.event
 async def on_ready():
-    print("Bot is ready")
+	print("Bot is ready")
 
 @client.command()
 async def slap(ctx, *, reason: Slapper):
-    await ctx.send(reason)
-    
+	await ctx.send(reason)
+
 @client.command()
 async def slaphard(ctx, members: commands.Greedy[discord.Member], *, reason='no reason'):
     slapped = " and ".join(x.name for x in members)
@@ -53,5 +53,69 @@ async def add_roles(ctx):
     member = ctx.message.author
     role = discord.utils.get(member.guild.roles, name="The crocodile hunter")
     await discord.Member.add_roles(member, role)
+
+@client.event
+async def on_member_join(member):
+    with open('users.json', 'r') as f:
+        users = json.load(f)
+
+    await update_data(users, member)
+
+    with open('users.json', 'w') as f:
+        json.dump(users, f)
+
+
+@client.event
+async def on_message(message):
+    if message.author.bot == False:
+        with open('users.json', 'r') as f:
+            users = json.load(f)
+
+        await update_data(users, message.author)
+        await add_experience(users, message.author, 5)
+        await level_up(users, message.author, message)
+
+        with open('users.json', 'w') as f:
+            json.dump(users, f)
+
+    await client.process_commands(message)
+
+
+async def update_data(users, user):
+    if not f'{user.id}' in users:
+        users[f'{user.id}'] = {}
+        users[f'{user.id}']['experience'] = 0
+        users[f'{user.id}']['level'] = 1
+
+
+async def add_experience(users, user, exp):
+    users[f'{user.id}']['experience'] += exp
+
+
+async def level_up(users, user, message):
+    with open('levels.json', 'r') as g:
+        levels = json.load(g)
+    experience = users[f'{user.id}']['experience']
+    lvl_start = users[f'{user.id}']['level']
+    lvl_end = int(experience ** (1 / 4))
+    if lvl_start < lvl_end:
+        await message.channel.send(f'{user.mention} has leveled up to level {lvl_end}')
+        users[f'{user.id}']['level'] = lvl_end
+
+@client.command()
+async def level(ctx, member: discord.Member = None):
+    if not member:
+        id = ctx.message.author.id
+        with open('users.json', 'r') as f:
+            users = json.load(f)
+        lvl = users[str(id)]['level']
+        await ctx.send(f'You are at level {lvl}!')
+    else:
+        id = member.id
+        with open('users.json', 'r') as f:
+            users = json.load(f)
+        lvl = users[str(id)]['level']
+        await ctx.send(f'{member} is at level {lvl}!')
+
     
 client.run(token)
